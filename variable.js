820927324f6474a80b387592149c79f2402336ba
Soyab <soyabgadekar2@gmail.com>
2026-09-17 19:41:09 +0530

    lerned about data types and creating variables

---
+++ variable.js
@@ -0,0 +1,17 @@
+let name = "Soyab"
+name = "Soyab Gadekar"
+console.log(name)
+
+let age = 22
+console.log(age)
+
+const percentage = 98.9
+
+const isStudent = true
+// isStudent = false - The const value cannot be changed
+console.log(isStudent)
+
+console.log(typeof(name))
+console.log(typeof(age))
+console.log(typeof(percentage))
+console.log(typeof(isStudent))
